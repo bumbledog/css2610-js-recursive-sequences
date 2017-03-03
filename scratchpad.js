f5512c930c2c785dddb4bebde9e88552b6999b01
Kylie Williamson <kyliewill@rocketmail.com>
2017-03-03 12:19:14 -0700

    fib, tri, and pell tree done

--- scratchpad.js
+++ scratchpad.js
@@ -1,8 +1,6 @@
 document.getElementsByTagName("title")[0].innerHTML = "JavaScript Recursive Sequences";
 
-var fibDiv = Div('fibonacci');
-
-fibonacci(11, document.querySelector('#fibonacci'));
+fibonacci(11);
 pell(11);
 tribonacci(11);
 
@@ -19,57 +17,114 @@ document.head.appendChild(test);*/
 ///////////////////////////
 //  The Geometric Functions
 ///////////////////////////
-function fibonacci(n, node){
+function fibonacci(n){
+  var fibDiv = Div('fibonacci');
   let fibTree = fib(n);
-  node.appendChild(fibTree.html);
+  fibDiv.appendChild(fibTree.html);
 }
 
 function fib(n){
   let div = document.createElement('div');
   div.setAttribute('class', 'fib');
+  div.setAttribute('class', 'tree');
   let p = document.createElement('p');
-  let value = 0;
 
   //base case
   if (n <= 1){
-    value = n;
-  }else{
-    var left = fib(n - 1);
-    var right = fib(n - 2);
-    value = left.value + right.value;
-    var hasChildren = true;
+    p.textContent = 'Fib(' + n + ') = ' + n;
+    div.appendChild(p);
+    return {value: n, html: div};
   }
 
+  let left = fib(n - 1);
+  let right = fib(n - 2);
+  let value = left.value + right.value;
   p.textContent = 'Fib(' + n + ') = ' + value;
+
+  let subDiv = document.createElement('div');
+  subDiv.setAttribute('class','inner');
+
   div.appendChild(p);
-
-  if(hasChildren){
-    let subDiv = document.createElement('div');
-    subDiv.setAttribute('class','inner');
-
-    subDiv.appendChild(left.html);
-    subDiv.appendChild(right.html);
-    div.appendChild(subDiv);
-  }
+  subDiv.appendChild(left.html);
+  subDiv.appendChild(right.html);
+  div.appendChild(subDiv);
 
   return {value: value, html: div};
 }
 
 function pell(n){
+  var pDiv = Div('pell')
+  let pellTree = pe(n);
+  pDiv.appendChild(pellTree.html);
+}
+
+function pe(n){
+  let div = document.createElement('div');
+  div.setAttribute('class', 'pell');
+  div.setAttribute('class', 'tree');
+  let p = document.createElement('p');
+
   if (n <= 1){
-    return n;
+    p.textContent = 'pell(' + n + ') = ' + n;
+    div.appendChild(p);
+    return {value: n, html: div};
   }
-  return 2 * pell(n - 1) + pell(n - 2);
+
+  let left = pe(n-1);
+  let right = pe(n-2);
+  let value = 2 * left.value + right.value;
+  p.textContent = 'pell(' + n + ') = ' + value;
+
+  let subDiv = document.createElement('div');
+  subDiv.setAttribute('class','inner');
+
+  div.appendChild(p);
+  subDiv.appendChild(left.html);
+  subDiv.appendChild(right.html);
+  div.appendChild(subDiv);
+
+  return {value: value, html: div};
 }
 
 function tribonacci(n){
+  var triDiv = Div('tribonacci')
+  let triTree = tri(n);
+  triDiv.appendChild(triTree.html);
+}
+
+function tri(n){
+  let div = document.createElement('div');
+  div.setAttribute('class', 'tri');
+  div.setAttribute('class', 'tree');
+  let p = document.createElement('p');
+
   if (n <= 1){
-    return 1;
+    p.textContent = 'Tri(' + n + ') = ' + 0;
+    div.appendChild(p);
+    return {value: 0, html: div};
   }
   if (n === 2){
-    return 1;
+    p.textContent = 'Tri(' + n + ') = ' + 1;
+    div.appendChild(p);
+    return {value: 1, html: div};
   }
-  return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+
+  let left = tri(n-1);
+  let mid = tri(n-2);
+  let right = tri(n-3);
+  let value = left.value + mid.value + right.value;
+  p.textContent = 'tri(' + n + ') = ' + value;
+
+  let subDiv = document.createElement('div');
+  subDiv.setAttribute('class','inner');
+
+  div.appendChild(p);
+  subDiv.appendChild(left.html);
+  subDiv.appendChild(mid.html);
+  subDiv.appendChild(right.html);
+  div.appendChild(subDiv);
+
+  return {value: value, html: div};
 }
 
 //creates unique divs
@@ -77,4 +132,5 @@ function Div(id){
   let div = document.createElement('div');
   div.setAttribute('id', id);
   document.body.appendChild(div);
+  return div;
 }
